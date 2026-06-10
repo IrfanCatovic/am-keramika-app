102c0e204726e5f4ec8f6bcd5037ae6f6868e499
IrfanCatovic <catovicc84@gmail.com>
2026-06-10 11:15:53 +0200

    Add product repository functions for creating and retrieving products

--- backend/repositories/product_repository.go
+++ backend/repositories/product_repository.go
@@ -1,1 +1,21 @@
 package repositories
+
+import (
+	"am-keramika-backend/database"
+	"am-keramika-backend/models"
+)
+
+func CreateProduct(product *models.Product) error {
+	result := database.DB.Create(&product)
+
+	return result.Error	
+}
+
+func GetAllProducts() ([]models.Product, error) {
+  var products []models.Product
+
+  result := database.DB.Preload("Category").Find(&products)
+
+  return products, result.Error
+}
+
