2946ba7dff2ccf7ce1bae2b2f299ef01c5b2ffe1
IrfanCatovic <catovicc84@gmail.com>
2026-07-04 23:11:09 +0200

    Refactor CreatePayment function to improve error handling and validation. Update error messages for clarity, implement customer debt checks, and ensure proper transaction management during payment creation and allocation updates. Enhance data integrity by preloading related models upon payment retrieval.

--- backend/repositories/payment_repository.go
+++ backend/repositories/payment_repository.go
@@ -30,7 +30,7 @@ func CreatePayment(req dto.CreatePaymentRequest, createdByUserID uint) (models.P
 	//Provera da li postoji slice sa raspodelom racuna i da li postoji duplikat raspodele racuna
 	if len(req.Allocations) == 0 {
 		tx.Rollback()
-		return models.Payment{}, errors.New("Uplata mora imati bar jedna racun")
+		return models.Payment{}, errors.New("uplata mora imati bar jedan račun")
 	}
 
 	seenInvoiceIDs := make(map[uint]bool)
@@ -70,15 +70,15 @@ func CreatePayment(req dto.CreatePaymentRequest, createdByUserID uint) (models.P
 
 		remainingAmount := invoice.TotalAmount - invoice.PaidAmount
 
-		if allocationReq.Amount > remainingAmount {
+		if allocationReq.Amount > remainingAmount { //provera da li je iznos prenosenja veci od ostatka racuna
 			tx.Rollback()
-			return models.Payment{}, errors.New("iznos prenosenja ne moze biti veci od ostatka racuna")
+			return models.Payment{}, errors.New("iznos uplate ne može biti veći od preostalog duga računa")
 		}
 
-		invoice.PaidAmount += allocationReq.Amount //ovo je lokalna promena, ne utice na bazu
+		invoice.PaidAmount += allocationReq.Amount //ovo je lokalna promena, ne utice na bazu, dodajemo iznos prenosenja na iznos placenog racuna	
 
 		//sa frontenda nam stize 300, a duzni smo 300 za taj racun onda se uradi, a nije placeno nista 
-		//preostala vrednost je 0 i proverimo da li je to sto nam je stiglo sa frontenda vece od preostalog racuna, naravno da nije jer u jednaki ili preostalo vise
+		//znaci 300 je dug - 0 (koliko je vec otplaceno) = 300  
 		//invoice.paidAmount dodajemo mi ovo sad sto nam je stiglo sa frontenda i proveravamo status ako je invoice.PaindAmount jednak invoice.TotalAmount 
 		//onda znaci da je placeno i totalni racun isto tako da je placen, ako nije isto onda je delimicno placen
 		if invoice.PaidAmount == invoice.TotalAmount {
@@ -91,9 +91,66 @@ func CreatePayment(req dto.CreatePaymentRequest, createdByUserID uint) (models.P
 		invoicesToUpdate = append(invoicesToUpdate, invoice) //racuni koje cemo posle da azuriramo da ih ne bi cuvali jedan po jedan
 	}
 
+	payment := models.Payment{
+		CustomerID: customer.ID,
+		CreatedByUserID: createdByUserID,
+		TotalAmount: totalAmount,
+	}
+	if err := tx.Create(&payment).Error; err != nil {
+		tx.Rollback()
+		return models.Payment{}, err
+	}
 
+	for _, allocationReq := range req.Allocations {
+		allocation := models.PaymentAllocation{
+			PaymentID: payment.ID,
+			InvoiceID: allocationReq.InvoiceID,
+			Amount: allocationReq.Amount,
+		}
+		allocationsToCreate = append(allocationsToCreate, allocation)
+	}
 
-	tx.Rollback()
+	if err := tx.Create(&allocationsToCreate).Error; err != nil {
+		tx.Rollback()
+		return models.Payment{}, err
+	}
 
-	return models.Payment{}, errors.New("create payment nije jos implementiran")
+	for _, invoice := range invoicesToUpdate {
+		if err := tx.Model(&models.Invoice{}).
+		 	Where("id = ?", invoice.ID).
+			Updates(map[string]interface{}{
+				"paid_amount": invoice.PaidAmount,
+				"status": invoice.Status,
+			}).Error; err != nil {
+			tx.Rollback()
+			return models.Payment{}, err
+		}
+	}
+	
+	newCustomerDebt := customer.TotalDebt - totalAmount
+	if newCustomerDebt < 0 {
+		tx.Rollback()
+		return models.Payment{}, errors.New("kupac ne moze imati negativan dug")
+	}
+ 
+	if err := tx.Model(&models.Customer{}).
+		Where("id = ?", customer.ID).
+		Update("total_debt", newCustomerDebt).Error; err != nil {
+		tx.Rollback()
+		return models.Payment{}, err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return models.Payment{}, err
+	}
+
+	var createdPayment models.Payment
+	if err := database.DB.Preload("Customer").
+	Preload("CreatedByUser").
+	Preload("Allocations").
+	Preload("Allocations.Invoice").
+	First(&createdPayment, payment.ID).Error; err != nil {
+		return payment, nil
+	}
+	return createdPayment, nil
 }
