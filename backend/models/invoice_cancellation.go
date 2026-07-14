9b55d3d925a0959af00ad619ad472a2be421d751
IrfanCatovic <catovicc84@gmail.com>
2026-07-14 21:38:35 +0200

    Implement logic to handle payment creation for invoices with nil CustomerID. Enhance payment processing by ensuring payments are automatically created and allocated to invoices lacking a customer, improving overall efficiency.

---
+++ backend/models/invoice_cancellation.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+type InvoiceCancellation struct {
+	gorm.Model
+	InvoiceID uint `gorm:"not null;uniqueIndex"`
+	Invoice Invoice `gorm:"foreignKey:InvoiceID"`
+
+	CreatedByUserID uint
+	CreatedByUser User `gorm:"foreignKey:CreatedByUserID"`
+
+	Reason string `gorm:"not null"`
+	DebtReducedAmount float64 `gorm:"not null;default:0"`
+	RefundAmount float64 `gorm:"not null;default:0"`
+}
+
